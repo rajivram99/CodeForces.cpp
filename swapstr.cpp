99831b6ad64c11022fa39534e8037e99ca5cd7e1
rajiv77777 <44100247+rajiv77777@users.noreply.github.com>
2020-10-08 10:52:51 +0530

    swap string without strrev()

---
+++ swapstr.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countDigits(int n)
+{
+	return floor(log10(n)+1);
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	int cd = countDigits(n);	
+     cout<<cd;
+	return 0;
+}
